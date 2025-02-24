e745c0925158b7b12da85564a6d8e336b6473cda
portiagalang1616 <galangportia_bsit@plmun.edu.ph>
2025-02-24 20:49:44 +0800

    Submitting OBJPROG Week 05 - Laboratory # 16

---
+++ MethodOverloading.java
@@ -0,0 +1,23 @@
+public class MethodOverloading {  
+    public static void main(String[] args) {
+        printValue(10);        // Calls int version
+        printValue(3.14);      // Calls double version
+        printValue("Hello!");  // Calls String version
+    }
+
+    
+    public static void printValue(int number) {
+        System.out.println("Integer value: " + number);
+    }
+
+   
+    public static void printValue(double number) {
+        System.out.println("Double value: " + number);
+    }
+
+    
+    public static void printValue(String text) {
+        System.out.println("String value: " + text);
+    }
+
+} 
